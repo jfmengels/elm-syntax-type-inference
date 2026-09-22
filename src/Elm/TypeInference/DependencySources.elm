5a5fbac47384d026b3e3dfc562f38b71bb2a49a0
Jeroen Engels <jfm.engels@gmail.com>
2026-09-22 12:32:10 +0200

    Move filtering condition to earlier fold

--- src/Elm/TypeInference/DependencySources.elm
+++ src/Elm/TypeInference/DependencySources.elm
@@ -127,7 +127,7 @@ neededSources deps sources =
                         docsModuleRefs pkg.modules
                             |> List.foldl
                                 (\(( m, _ ) as ref) acc ->
-                                    if isKnownRef docsTypes ref then
+                                    if isKnownRef docsTypes ref || Set.member m supplied then
                                         acc
 
                                     else
@@ -135,14 +135,7 @@ neededSources deps sources =
                                 )
                                 Set.empty
                             |> Set.toList
-                            |> List.filterMap
-                                (\m ->
-                                    if Set.member m supplied then
-                                        Nothing
-
-                                    else
-                                        Just (ModuleNameExtra.dottedToFilePath m)
-                                )
+                            |> List.map ModuleNameExtra.dottedToFilePath
                 in
                 case remaining of
                     [] ->
