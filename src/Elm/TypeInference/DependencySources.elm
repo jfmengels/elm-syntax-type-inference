4f1d19a61971c07bf6974c4e2aba8c4079ba449a
Jeroen Engels <jfm.engels@gmail.com>
2026-09-22 11:25:11 +0200

    Combine Set.toList and List.map into a fold

--- src/Elm/TypeInference/DependencySources.elm
+++ src/Elm/TypeInference/DependencySources.elm
@@ -140,7 +140,7 @@ neededSources deps sources =
 
                 else
                     ( package
-                    , List.map ModuleNameExtra.dottedToFilePath (Set.toList remaining)
+                    , Set.foldr (\m acc -> ModuleNameExtra.dottedToFilePath m :: acc) [] remaining
                     )
                         :: needsSourcesAcc
             )
